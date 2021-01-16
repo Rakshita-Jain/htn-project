e554c9251841f6a9a19231538a6bb9f3ff128e66
Rakshita-Jain <jainrakshita@gmail.com>
2021-01-16 14:41:36 -0700

    this is the message

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <div className= "App">
       <h1 className="welcome">Welcome to Healthtech!</h1>
+      <div className="background-top"></div>
       <div className="doctors">
 
       </div>
@@ -12,10 +13,16 @@ const App = () => {
       <p className="button-click">Click the button below to chat with our</p> 
       <p className="button-click"> bot and find the best hospital near you</p>
       <div className="button">
+        <div class="fb-customerchat"
+                 page_id="<PAGE_ID>">
           <div className="button-text">
             CHAT NOW
           </div>
+        </div>
+          
       </div>
+
+      <div className="background-bottom"></div>
      
       <Map/>
      </div>
