d3050ac5de1a5f7ae11f15daf9243112e7ad25c5
Rakshita-Jain <jainrakshita@gmail.com>
2021-01-16 13:16:46 -0700

    Merge branch 'main' of https://github.com/Rakshita-Jain/htn-project
    Co-authored-by: Tahjeira Cameron <t4camero@uwaterloo.ca>

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Map } from './components/Map';
 const App = () => {
   return (
     <div className= "App">
-      <h1 className="welcome">Welcome to</h1>
+      <h1 className="welcome">Welcome to Healthtech!</h1>
       <div className="doctors">
-       <img id="main-pic" alt="doctors"></img>
+
       </div>
-      
-      <p className="button-click">Click the button below to chat with our bot and find the best hospital near you</p>
+    
+      <p className="button-click">Click the button below to chat with our</p> 
+      <p className="button-click"> bot and find the best hospital near you</p>
       <div className="button">
           <div className="button-text">
             CHAT NOW
